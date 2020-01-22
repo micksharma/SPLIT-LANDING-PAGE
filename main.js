5a0af0ede270c571f0e0fadb994464f7d4b78f79
Abhishek <mick.sharma02@gmail.com>
2020-01-22 19:31:36 +0530

    SPLIT LANDING PAGE

---
+++ main.js
@@ -0,0 +1,7 @@
+const left = document.querySelector('.left');
+const left = document.querySelector('.right');
+const left = document.querySelector('.container');
+
+left.addEventListener('mouseenter',()=>{
+     Container.classList.add('hover-left');
+})
